25024ded63e658f293903cf97da3de1f2b16f436
alumnosidat13 <alumnosidat13@gmail.com>
2025-02-20 19:49:58 -0500

    avanzado del proyecto

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import model.Alumno;
+import model.Curso;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Creando un objeto desde un constructor vacio
+        Curso objCurso = new Curso();
+        objCurso.setIdcurso(1);
+        System.out.println(objCurso.getIdcurso());
+        //Creando un objeto desde un constructor con parametros
+        Curso objCurso2 = new Curso(2);
+        System.out.println(objCurso2.getIdcurso());
+        Alumno objAlumno = new Alumno();
+        objAlumno.setNombre("Luis");
+        objAlumno.setApellido("Salvat");
+        objAlumno.setDni("454858");
+        objAlumno.setEdad(32);
+        objAlumno.setId(1);
+
+
+    }
+}
