0e8f2c7804588262b895455023420ba8907febeb
alumnosidat13 <alumnosidat13@gmail.com>
2025-02-20 20:20:25 -0500

    aplicando polimorfismo a la clase alumno y docente

--- src/model/Alumno.java
+++ src/model/Alumno.java
@@ -2,4 +2,10 @@ package model;
 
 public class Alumno extends Persona {
 
+    @Override
+    public boolean registrarAsistencia(String codIdentificador, String fechaAsistencia, String HoraAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar lógica " +
+                "para el alumno");
+        return false;
+    }
 }
--- src/model/Docente.java
+++ src/model/Docente.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Docente extends Persona {
+
+    @Override
+    public boolean registrarAsistencia(String codIdentificador, String fechaAsistencia, String HoraAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar lógica " +
+                "para el docente");
+        return true;
+    }
 }
